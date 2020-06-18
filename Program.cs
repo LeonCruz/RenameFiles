80c14debde1966733e156b06087662b280f04577
Leon Cruz <leon.cruz.teixeira@gmail.com>
2020-06-18 00:06:45 -0300

    Validação do caminho passado pelo argumento

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+
 
 namespace rename_files
 {
@@ -6,7 +8,17 @@ namespace rename_files
     {
         static void Main(string[] args)
         {
-	    Console.WriteLine("Hello, World");
+	    String directory = args[0];
+
+	    if (Directory.Exists(directory))
+	    {
+	      Console.WriteLine("É uma pasta"); 
+            }
+	    else 
+	    {
+	      Console.WriteLine("Não é uma pasta");
+	      Console.WriteLine("Insira um caminho válido");
+	    }
         }
     }
 }
