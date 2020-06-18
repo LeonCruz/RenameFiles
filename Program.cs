860bd227f5881202fbdd69943e79e46ce5f0f7ab
Leon Cruz <leon.cruz.teixeira@gmail.com>
2020-06-18 01:47:33 -0300

    Função que renomeia os arquivos

--- Program.cs
+++ Program.cs
@@ -7,25 +7,51 @@ namespace rename_files
     class Program
     {
 
-	private static void getFiles(string path)
+	private static string[]  getFiles(string path)
 	{
             string[] files = Directory.GetFiles(path);
             Array.Sort(files);    
 
-	    foreach (string file in files)
-	    {
-	      Console.WriteLine(file);
+	    return files;
+	}
+
+	private static void renameFiles(string directory, string[] files, string pattern)
+	{
+	  for (int i=0; i<files.Length; i++)
+	  {
+	    string newName = "";
+	    if (i < 9)
+	    { 
+	      newName = pattern.Replace("\\d", "0" + (i+1));
 	    }
+	    else
+	    {
+	      newName = pattern.Replace("\\d", "" + (i+1));
+	    }
+
+	    FileInfo information = new FileInfo(files[i]);
+            newName = newName + information.Extension;
+
+	    string fullPath = Path.Combine(directory, newName);
+
+	    Console.WriteLine(fullPath);
+	    File.Move(files[i], fullPath);
+	  }
+
+	  Console.WriteLine("Concluído.");
 	}
 
 
         static void Main(string[] args)
         {
 	    string directory = args[0];
+	    string pattern = args[1];
+	    string[] files;
 
 	    if (Directory.Exists(directory))
 	    {
-	      getFiles(directory);
+	      files = getFiles(directory);
+	      renameFiles(directory, files, pattern);
             }
 	    else 
 	    {
